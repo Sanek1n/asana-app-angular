2aa6d9392b76716123073e6decd369f1aedd775b
Sanek1n <sanekin8ru@outlook.com>
2024-04-11 15:14:02 +0400

    feat: add messages for empty tasks, add some styles

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -12,7 +12,7 @@ function filterCurrentTasks(tasks: Task[]): Task[] {
 }
 
 function filterComingTasks(tasks: Task[]): Task[] {
-  return tasks.filter((val) => (new Date(val.beginDate) > new Date() && !val.ended));
+  return tasks.filter((val) => (new Date(val.deadline) < new Date() && !val.ended));
 }
 
 function filterEndedTasks(tasks: Task[]): Task[] {
@@ -27,12 +27,21 @@ function filterEndedTasks(tasks: Task[]): Task[] {
 export class MainPageComponent {
   public currentDate: Date;
 
+  public countTasks: number = 0;
+
+  public showButtonAdd = true;
+
   public taskItems: Observable<Task[]>;
 
   constructor(private dataSource: RepositoryService) {
     this.currentDate = new Date();
     this.taskItems = this.dataSource.getTasks()
       .pipe(map((el) => filterCurrentTasks(el)));
+    this.taskItems.subscribe({
+      next: (data) => {
+        this.countTasks = data.length;
+      },
+    });
   }
 
   handlerChange(event: MatTabChangeEvent) {
@@ -40,17 +49,25 @@ export class MainPageComponent {
       case 0:
         this.taskItems = this.dataSource.getTasks()
           .pipe(map((el) => filterCurrentTasks(el)));
+        this.showButtonAdd = true;
         break;
       case 1:
         this.taskItems = this.dataSource.getTasks()
           .pipe(map((el) => filterComingTasks(el)));
+        this.showButtonAdd = false;
         break;
       case 2:
         this.taskItems = this.dataSource.getTasks()
           .pipe(map((el) => filterEndedTasks(el)));
+        this.showButtonAdd = false;
         break;
       default:
     }
+    this.taskItems.subscribe({
+      next: (data) => {
+        this.countTasks = data.length;
+      },
+    });
   }
 
   completeTask(event: Task) {
